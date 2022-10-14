4813741aaf3a04d57dc57a40d2038cc0b6224a25
AlanDiCha123 <didier.ominguez@gmail.com>
2022-10-14 12:22:39 -0500

    moving the gross function to Algoritmo file

--- src/mx/unam/algoritmo/Algoritmo.java
+++ src/mx/unam/algoritmo/Algoritmo.java
@@ -2,6 +2,7 @@ package mx.unam.algoritmo;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 
 public class Algoritmo {
 
@@ -10,21 +11,23 @@ public class Algoritmo {
     // * This is the number to compare
     private int[] compar = MixRandNums.mixDiffRandDigits();
     // * These are the counters for each attempt
-    private byte count_right = 0;
-    private byte count_wrong = 0;
+    private int count_right = 0;
+    private int count_wrong = 0;
     // * This matrix saves all the attempts and the counters too.
     private int[][] matrix = new int[9][4];
     // * This matrix saves all the wrong and right number of digits
-    private int[] counters_right = new int[9];
-    private int[] counters_wrong = new int[9];
-    // * This is a flag that indicates if the number generated is right
-    private boolean band = false;
-    // * This array save all number that are considerated as droped
-    private ArrayList<Integer> dropped = new ArrayList<>();
+    private ArrayList<Integer> counters_right = new ArrayList<>();
+    private ArrayList<Integer> counters_wrong = new ArrayList<>();
+    // * This is a flag that indicates if the whole number generated is right
+    private boolean is_right = false;
     // * This array save all pre-pinned numbers 
     private ArrayList<Integer> prepin_nmbs = new ArrayList<>();
     // * This array save all pinned numbers 
-    // private ArrayList<Integer> pinned_nmbs = new ArrayList<>();
+    private ArrayList<Integer> pinned_nmbs = new ArrayList<>();
+    // * This arrray save all not pinned numbers
+    private ArrayList<Integer> not_pinned_nmbs = new ArrayList<>();
+    // * This array save all numbers to choose
+    private ArrayList<Integer> to_choose = new ArrayList<>();
     // * This flag save all pinned numbers and won't change it
     private boolean set_pinned_numbers = false;
     // * This counter is for each intent
@@ -34,6 +37,7 @@ public class Algoritmo {
 
     public Algoritmo(int[] number){
         this.number = number;
+        for (int i = 0; i < 10; i++) to_choose.add(i);
     }
 
     public Algoritmo(){}
@@ -46,20 +50,20 @@ public class Algoritmo {
         while (counter <= 9) {
             System.out.println("||Ciclo: " + counter + " ||");
             if (checkNumEquals(number, compar)) {
-                band = true;
+                is_right = true;
                 break;
             } else {
                 for (int i = 0; i < compar.length; i++) {
                     matrix[counter - 1][i] = compar[i];
                 }
-                counters_right[counter - 1] = count_right;
-                counters_wrong[counter - 1] = count_wrong;
+                counters_right.add(count_right);
+                counters_wrong.add(count_wrong);
                 printOut(compar);
                 compar = modifyDigit(compar, counter);
                 counter++;
             } 
         }
-        if (band){
+        if (is_right){
             System.out.println("I did it :)");
         } else{
             System.out.println("I couldn't solve it. So sorry :(");
@@ -76,7 +80,7 @@ public class Algoritmo {
         count_right = 0;
         count_wrong = 0;
         ArrayList<Integer> aux_first = new ArrayList<>();
-        boolean band_first = false;
+        boolean is_right_first = false;
         for (int i = 0; i < 4; i++) {
             if (MixRandNums.contains(first, second[i])){
                 for (int j = 0; j < 4; j++) {
@@ -84,7 +88,7 @@ public class Algoritmo {
                         if (prematch_first[j] || prematch_second[i]){
                             prematch_first[j] = false;
                             prematch_second[i] = false;
-                            if (band_first){
+                            if (is_right_first){
                                 prematch_first[aux_first.get(0)] = false;
                             }
                             count_wrong--;
@@ -105,12 +109,12 @@ public class Algoritmo {
                             prematch_first[j]  = true;
                             prematch_second[i] = true;
                             aux_first.add(j);
-                            band_first = true;
+                            is_right_first = true;
                         }
                     }
                 }
             } 
-            band_first = false;
+            is_right_first = false;
             aux_first.clear();
         }
         return count_right == 4;
@@ -130,21 +134,20 @@ public class Algoritmo {
         // Creating the modified number
         int[] modified = new int[4];
         // Highlighting the current counters
-        int rnod = counters_right[intent - 1];
-        int wnod = counters_wrong[intent - 1];
+        int rnod = counters_right.get(intent - 1);
+        int wnod = counters_wrong.get(intent - 1);
 
         // ! If W + R == 4. Focus on these digits and dropped the others
         if (wnod + rnod != 4) {
             if (wnod == 0 && rnod == 0) {
             // * If no number doesn't equals to the number to find
                 for (int i = 0; i < 4; i++) {
-                    dropped.add(compare[i]);
+                    to_choose.remove((Object)compare[i]);
                 }
                 if (counter == 1){
-                    modified = MixRandNums.mixDiffRandDigits(dropped);
+                    modified = MixRandNums.mixDiffRandDigits(to_choose);
                 } else {
-                    modified = MixRandNums.mixDiffRandDigits(dropped, 
-                        matrix, compare, counters_right, counters_wrong, intent);
+                    modified = mixDiffRandDigits();
                 }
             } else { // When W, R != 0
                 if (counter == 1){
@@ -156,8 +159,7 @@ public class Algoritmo {
                         }
                     }
                     if (!change){
-                        modified = MixRandNums.mixDiffRandDigits(dropped, 
-                        matrix, compare, counters_right, counters_wrong, intent);
+                        modified = mixDiffRandDigits();
                     } else {
                         // TODO: Implement the number to repeat
                         // TODO: Complete the repeatDigits function
@@ -173,16 +175,38 @@ public class Algoritmo {
                 }
                 set_pinned_numbers = true;
             }
-            modified = MixRandNums.sortDigits(matrix, prepin_nmbs);
+            modified = sortDigits(prepin_nmbs);
         }
         return modified;
     }
 
     public boolean checkSumCounters(){
-        for (int i = 0; i < counters_right.length; i++) {
-            if (counters_right[i] + counters_wrong[i] >= 3)
+        for (int i = 0; i < 5; i++) {
+            if (counters_right.get(i) + counters_wrong.get(i) >= 3)
             return true;
         }
         return false;
     }
+
+    public int[] mixDiffRandDigits(){
+        // Taking care of array previous  and (R, W)
+        final int TEN = 10;
+        int[] modified = new int[4];
+        int count_Ri;
+        int count_W;
+        int count_Ra = 0;
+        // * Index to remove from matrix[2][]
+        ArrayList<Integer> index = new ArrayList<>();
+        for (int i = 0; i < modified.length; i++) index.add(i);
+
+        return modified;
+    }
+
+    public int[] sortDigits(ArrayList<Integer> p_pinned){
+        int[] modified = new int[4];
+        // TODO: Complete the function
+        // sort the digits taking care of matrix
+        return modified;
+    }
+
 }
--- src/mx/unam/algoritmo/MixRandNums.java
+++ src/mx/unam/algoritmo/MixRandNums.java
@@ -13,13 +13,11 @@ public class MixRandNums {
     public static int[] mixRandDigits(){
         Random rand = new Random();
         int[] numero = new int[4];
-
         int my_number = rand.nextInt(TEN_THOUSAND);
         numero[0] = my_number / 1000;
         numero[1] = (my_number / 100) % 10;
         numero[2] = (my_number / 10 ) % 10;
         numero[3] = my_number % 10;
-
         return numero;
     }
 
@@ -45,126 +43,12 @@ public class MixRandNums {
 
     public static int[] mixDiffRandDigits(ArrayList<Integer> dropped){
         int[] num = new int[4];
-        ArrayList<Integer> list = new ArrayList<>();
-        for (int i = 0; i < TEN; i++) list.add(i);
-        list.removeIf(x -> dropped.contains(x));
-        Collections.shuffle(list);
-        for (int i = 0; i < num.length; i++) num[i] = list.get(i);
+        Collections.shuffle(dropped);
+        for (int i = 0; i < num.length; i++) num[i] = dropped.get(i);
         return num;
-    }
-
-    public static int[] mixDiffRandDigits(ArrayList<Integer> dropped, 
-        int[][] matrix, int[] array, int[] right, int[] wrong, int intent){
-            // TODO: Complete the function
-            // Taking care of array previous  and (R, W)
-        int[] modified = new int[4];
-        boolean empty_dropped = true;
-        int count_Ri;
-        int count_W;
-        int count_Ra = 0;
-        // * Index to remove from matrix[2][]
-        ArrayList<Integer> index = new ArrayList<>();
-        for (int i = 0; i < modified.length; i++) index.add(i);
-
-        if (!dropped.isEmpty()){
-            ArrayList<Integer> list = new ArrayList<>();
-            for (int i = 0; i < TEN; i++) list.add(i);
-            list.removeIf(x -> dropped.contains(x));
-            empty_dropped = false;
-        }
-        if (intent == 2) {
-            ArrayList<Integer> numbers = new ArrayList<>();
-            ArrayList<Integer> removed = new ArrayList<>();
-            for (int i = 0; i < 2; i++) {
-                for (int j = 0; j < modified.length; j++) numbers.add(j);
-                count_Ri = right[i];
-                count_W = wrong[i];
-                count_Ra = count_Ra + (count_Ri + count_W);
-                if (count_Ri + count_W == 0){
-                    if (i == 0) continue;
-                    else if (i == 1){
-                    // Repeat digits (special case: 8 dropped in 2 intent)
-                        for (int j = 0; j < TEN; j++) removed.add(j);
-                        dropped.forEach(x -> removed.removeIf(n -> n == x));
-                        for (int j = 0; j < modified.length; j++) {
-                            Collections.shuffle(removed);
-                            modified[j] = removed.get(0);
-                        }
-                    }
-                } 
-                while (count_Ri > 0 || count_W > 0){
-                    if (count_Ri != 0){
-                        Collections.shuffle(index);
-                        modified[index.get(0)] = matrix[i][index.get(0)];
-                        removed.add(matrix[i][numbers.get(0)]); // 7
-                        index.remove(numbers.get(0));//rm index 2 obj from array
-                        numbers.remove(0); // del 2 index
-                        count_Ri--;
-                    } else if (count_W != 0){
-                        Collections.shuffle(numbers);
-                        Collections.shuffle(index);
-                        if (index.get(0) != numbers.get(0)){
-                            modified[index.get(0)] = matrix[i][numbers.get(0)];
-                            removed.add(matrix[i][numbers.get(0)]);
-                            numbers.remove(0);
-                        } else {
-                            modified[index.get(0)] = matrix[i][numbers.get(1)];
-                            removed.add(matrix[i][numbers.get(1)]);
-                            numbers.remove(1);
-                        }
-                        index.remove(0);
-                        count_W--;
-                    }
-                }
-                numbers.clear();
-            }
-            boolean my_band = false;
-            while (count_Ra < 4) {
-                if (!my_band){
-                    ArrayList<Integer> aux = new ArrayList<>();
-                    for (int i = 0; i < TEN; i++) numbers.add(i);
-                    for (int i = 0; i < 2; i++) {
-                        for (int j = 0; j < modified.length; j++) {
-                            aux.add(matrix[i][j]);
-                        }
-                    }
-                    aux.forEach(x -> numbers.removeIf(n -> x == n));
-                }
-                Collections.shuffle(numbers);
-                Collections.shuffle(index);
-                modified[index.get(0)] = numbers.get(0);
-                numbers.remove(0);
-                index.remove(0);
-                count_Ra++;
-                my_band = true;
-            }
-        } else {
-            // TODO: Implement the gross function
-        }
-        return modified;
     }
 
     public static boolean contains(final int[] arr, final int key){
         return Arrays.stream(arr).anyMatch(i -> i == key);
     }
-
-    public static int[] sortDigits(int[][] matrix, ArrayList<Integer> p_pinned){
-        int[] modified = new int[4];
-        // TODO: Complete the function
-        // sort the digits taking care of matrix
-        return modified;
-    }
-
-    public static boolean containsItemOnMatrix(int[][] matrix, int[] array,
-            int intent){
-        for (int j = intent - 1; j >= 0; j--) {
-            for (int j2 = 3; j2 >= 0; j2--) {
-                if (contains(array, matrix[j][j2])){
-                    return true;
-                }
-            }
-        }
-        return false;
-    }
-
 }
